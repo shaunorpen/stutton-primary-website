dafea28ae5dc99f5e753cfe7ab31ea5233b9a159
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-29 16:20:03 +0000

    Add body

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,80 @@ import React from 'react'
 function Home () {
     return (
         <div>
-            Home
+            <div className="bg-green-200 px-48 py-5">
+                <div className="text-xl italic font-semibold">Welcome to</div>
+                <h1 className="text-5xl font-bold text-center leading-tight">Stutton CEVC Primary School</h1>
+                <div className="text-xl italic text-right font-semibold">an Asset Education School</div>
+            </div>
+            <div className="bg-green-300 flex">
+                <div className="w-3/4 p-4">
+                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 mb-4">
+                        <h2 className="text-2xl font-bold mb-3">Welcome</h2>
+                        <p className="leading-relaxed">
+                            Stutton CEVC Primary School is a mixed infant and junior academy in Stutton, Suffolk. 
+                            Our staff are committed to providing the best education for all our pupils. 
+                            Every child matters to us, every lesson, every day. 
+                            Visits and tours are warmly welcomed anytime during the school year. 
+                            Please make an appointment through the school office.
+                        </p>
+                    </div>
+                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100">
+                        <h2 className="text-2xl font-bold mb-3">Latest News</h2>
+                        <div className="flex">
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                        </div>
+                        <div className="flex">
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                            <div className="shadow-outline mr-3 mb-3 p-4">
+                                <h3 className="font-bold text-lg">Title</h3>
+                                <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus nihil aspernatur, suscipit ducimus corporis eos deserunt accusamus provident perferendis autem!</p>
+                                <a href="#" className="bg-blue-500 hover:bg-blue-400 border rounded-lg px-4 py-1 mt-5 text-white text-sm text-bold inline-block">Read more...</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-1/4 py-4 pr-4">
+                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 mb-4">
+                        <h2 className="text-2xl font-bold mb-3">Upcoming Events</h2>
+                        <p className="text-center">
+                            Google Calendar
+                        </p>
+                    </div>
+                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100">
+                        <h2 className="text-2xl font-bold mb-3">Useful Links</h2>
+                        <ul>
+                            <li>Useful Link 1</li>
+                            <li>Useful Link 2</li>
+                            <li>Useful Link 3</li>
+                            <li>Useful Link 4</li>
+                            <li>Useful Link 5</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
