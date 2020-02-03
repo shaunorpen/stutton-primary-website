aa3fbe17b49eaac037c232c74306661b4ddbd5ab
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-03 19:38:45 +0000

    Add responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,16 @@ import img6 from '../images/Stutton-69-Edited-300x300.jpg'
 function Home () {
     return (
         <div>
-            <div className="bg-green-200 px-48 py-12">
-                <div className="text-xl italic font-semibold">Welcome to</div>
-                <h1 className="text-5xl font-bold text-center leading-tight">Stutton CEVC Primary School</h1>
-                <div className="text-xl italic text-right font-semibold">an Asset Education School</div>
+            <div className="bg-green-200 p-12 flex justify-center items-center">
+                <div>
+                    <div className="text-xl italic font-semibold">Welcome to</div>
+                    <h1 className="text-5xl font-bold text-center leading-tight px-6">Stutton CEVC Primary School</h1>
+                    <div className="text-xl italic text-right font-semibold">an Asset Education School</div>
+                </div>
             </div>
-            <div className="bg-green-300 flex">
-                <div className="w-3/4 p-4">
-                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 mb-4">
+            <div className="bg-green-300 flex flex-col lg:flex-row p-4">
+                <div className="w-full lg:w-3/4">
+                    <div className="shadow-lg rounded-lg p-4 bg-gray-100 mb-4">
                         <h2 className="text-2xl font-bold mb-3">Welcome</h2>
                         <p className="leading-relaxed mb-4">
                             Stutton Primary is a mixed infant and junior academy in Stutton, Suffolk. 
@@ -27,10 +29,10 @@ function Home () {
                             Please make an appointment through the school office.
                         </p>
                     </div>
-                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 pb-4">
+                    <div className="shadow-lg rounded-lg p-4 bg-gray-100 mb-4">
                         <h2 className="text-2xl font-bold mb-3">Latest News</h2>
-                        <div className="flex -mx-3">
-                            <div className="shadow w-1/3 flex flex-col justify-between mx-3 rounded-lg">
+                        <div className="flex flex-col lg:flex-row -mx-3">
+                            <div className="shadow w-auto lg:w-1/3 flex flex-col justify-between mx-3 rounded-lg">
                                 <div>
                                     <div style={{ backgroundImage: `url(${img1})`}} className="h-64 bg-cover rounded-t-lg" />
                                     <div className="p-3">
@@ -40,7 +42,7 @@ function Home () {
                                 </div>
                                 <button className="self-end bg-blue-400 px-2 py-1 text-white uppercase tracking-wide rounded mb-3 mr-3 text-sm hover:bg-blue-600">Read More</button>
                             </div>
-                            <div className="shadow w-1/3 flex flex-col justify-between mx-3 rounded-lg">
+                            <div className="shadow w-auto lg:w-1/3 flex flex-col justify-between mx-3 rounded-lg">
                                 <div>
                                     <div style={{ backgroundImage: `url(${img6})`}} className="h-64 bg-cover rounded-t-lg" />
                                     <div className="p-3">
@@ -50,7 +52,7 @@ function Home () {
                                 </div>
                                 <button className="self-end bg-blue-400 px-2 py-1 text-white uppercase tracking-wide rounded mb-3 mr-3 text-sm hover:bg-blue-600">Read More</button>
                             </div>
-                            <div className="shadow w-1/3 flex flex-col justify-between mx-3 rounded-lg">
+                            <div className="shadow w-auto lg:w-1/3 flex flex-col justify-between mx-3 rounded-lg">
                                 <div>
                                     <div style={{ backgroundImage: `url(${img3})`}} className="h-64 bg-cover rounded-t-lg" />
                                     <div className="p-3">
@@ -63,20 +65,20 @@ function Home () {
                         </div>
                     </div>
                 </div>
-                <div className="w-1/4 py-4 pr-4">
-                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 mb-4">
+                <div className="w-full lg:w-1/4 ml-4">
+                    <div className="shadow-lg rounded-lg p-4 bg-gray-100 mb-4">
                         <h2 className="text-2xl font-bold mb-3">Contact Us</h2>
                         <p className="mb-3">01473 328531</p>
                         <p className="mb-3 text-blue-600"><a href="mailto:admin@stuttonprimary.co.uk">admin@stuttonprimary.co.uk</a></p>
                         <p className="mb-3">Holbrook Road<br/>Stutton<br/>Suffolk<br/>IP9 2RY</p>
                     </div>
-                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 mb-4">
+                    <div className="shadow-lg rounded-lg p-4 bg-gray-100 mb-4">
                         <h2 className="text-2xl font-bold mb-3">Calendar</h2>
                         <p className="mb-3">
                             Google Calendar
                         </p>
                     </div>
-                    <div className="shadow-lg rounded-lg px-4 py-2 bg-gray-100 flex flex-col items-start">
+                    <div className="shadow-lg rounded-lg p-4 bg-gray-100 mb-4 flex flex-col items-start">
                         <h2 className="text-2xl font-bold mb-3">Newsletter</h2>
                         <input class="w-full px-2 py-1 rounded mb-3" placeholder="example@email.com"/>
                         <button class="bg-blue-400 mb-3 rounded px-3 py-2 uppercase tracking-wide text-white font-bold hover:bg-blue-600 self-end">Subscribe</button>
